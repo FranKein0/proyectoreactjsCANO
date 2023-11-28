bd52c497c972356a2ed657b31707c4bc7993c6a5
Francisco Cano <franffcano@gmail.com>
2023-11-28 19:33:07 -0300

    segundo commit

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,6 +2,7 @@ const CartWidget = () => {
     return (
         <div className="cart">
             <img src="./assets/img/cartwidget.png" alt="carrito" />
+            <p>3</p>
         </div>
     )
 }
